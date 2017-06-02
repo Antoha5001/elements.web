9a9cd6666cad306d651acb9129b463595b427452
Antoha5001 <plantsvzzombi@mail.ru>
2017-06-02 18:22:34 +0700

    menu for mobile

--- app/_menu/menu-01/menu-01.php
+++ app/_menu/menu-01/menu-01.php
@@ -33,49 +33,51 @@
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
   <style>
-
     html {}
-
-
   </style>
 </head>
 
 <body>
   <header>
     <nav class="menu">
-<ul>
-  <li><a href="#"><i class="fa fa-home"></i>Главная</a></li>
-  <li><a href="#"><i class="fa fa-shopping-basket"></i>Продукция</a>
-    <ul>
-      <li><a href="#">Одежда</a></li>
-      <li><a href="#">Электроника</a>
-        <ul>
-          <li><a href="#">Кофеварки</a></li>
-          <li><a href="#">Чайники</a>
-            <ul>
-              <li><a href="#">Rowenta</a></li>
-              <li><a href="#">Samsung</a></li>
-              <li><a href="#">Philips</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Мультиварки</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Продукты питания</a></li>
-      <li><a href="#">Инструменты</a></li>
-      <li><a href="#">Быт. химия</a></li>
-    </ul>
-  </li>
-  <li><a href="#"><i class="fa fa-cogs"></i>Услуги</a>
-    <ul>
-      <li><a href="#">Услуга 1</a></li>
-      <li><a href="#">Услуга 2</a></li>
-      <li><a href="#">Услуга 3</a></li>
-    </ul>
-  </li>
-  <li><a href="#"><i class="fa fa-newspaper-o"></i>Новости</a></li>
-  <li><a href="#"><i class="fa fa-address-book-o"></i>Контакты</a></li>
-</ul>
+      <input type="checkbox" name="toggle" id="menu" class="menu__toggle">
+      <label for="menu" class="menu__toggle"><i class="fa fa-bars"></i>Меню</label>
+      <ul>
+        <li><a href="#"><i class="fa fa-home"></i>Главная</a></li>
+        <li>
+          <input type="checkbox" name="toggle" class="submenu__toggle" id="sub_m1">
+          <a href="#"><i class="fa fa-shopping-basket"></i>Продукция</a>
+          <label for="sub_m1" class="submenu__toggle"><i class="fa"></i></label>
+          <ul>
+            <li><a href="#">Одежда</a></li>
+            <li><a href="#">Электроника</a>
+              <ul>
+                <li><a href="#">Кофеварки</a></li>
+                <li><a href="#">Чайники</a>
+                  <ul>
+                    <li><a href="#">Rowenta</a></li>
+                    <li><a href="#">Samsung</a></li>
+                    <li><a href="#">Philips</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Мультиварки</a></li>
+              </ul>
+            </li>
+            <li><a href="#">Продукты питания</a></li>
+            <li><a href="#">Инструменты</a></li>
+            <li><a href="#">Быт. химия</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-cogs"></i>Услуги</a>
+          <ul>
+            <li><a href="#">Услуга 1</a></li>
+            <li><a href="#">Услуга 2</a></li>
+            <li><a href="#">Услуга 3</a></li>
+          </ul>
+        </li>
+        <li><a href="#"><i class="fa fa-newspaper-o"></i>Новости</a></li>
+        <li><a href="#"><i class="fa fa-address-book-o"></i>Контакты</a></li>
+      </ul>
     </nav>
   </header>
   <script type="text/javascript" src="script/scripts.min.js"></script>
