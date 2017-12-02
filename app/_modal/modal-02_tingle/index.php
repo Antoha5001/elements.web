aaf230771d051d6463a264670e7e5fa7dce22717
Antoha5001 <plantsvzzombi@mail.ru>
2017-12-02 21:54:18 +0700

    sfa

---
+++ app/_modal/modal-02_tingle/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+	<link rel="stylesheet" href="tingle.min.css">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+	<button class="button" id="button">Нажми меня!</button>
+</div>
+
+<script src="jquery-3.2.1.min.js"></script>
+<script src="tingle.min.js"></script>
+<script src="jquery.inputmask.bundle.min.js"></script>
+<script src="script.js"></script>
+<!-- Yandex.Metrika counter --> <script type="text/javascript" > (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter46865814 = new Ya.Metrika({ id:46865814, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true, ut:"noindex" }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/46865814?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
+</body>
+</html>
