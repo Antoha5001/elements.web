d478b5e9e17e72d21f8d26698e8b768b7e8a9365
Antoha5001 <plantsvzzombi@mail.ru>
2018-01-12 20:32:24 +0700

    edit forms

---
+++ app/_forms/forms-4 constructor/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Title</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<form class="form-box__form validate" method="post" id="bottom_form">
+
+	<p class="form-box__zagolovok">Закажите звонок!</p>
+	<p class="form-box__podzagolovok">Отправте свой номер телефона и наш менеджер свяжится с Вами в
+		ближайшее время. </p>
+
+	<!--Name input -->
+	<div class="form-box__input-wrapper">
+		<label for="form_name" class="form-box__input-name">Как к вам обращаться</label>
+		<input class="form-box__input" type="text" placeholder="Имя" name="name" id="form_name"
+				 data-error="Не верный формат записи!">
+		<div class="message">Вы не ввели имя!</div>
+	</div>
+
+	<!--Phone input -->
+	<div class="form-box__input-wrapper">
+		<label for="form_phone" class="form-box__input-name">Номер телефона</label>
+		<input class="form-box__input" type="tel" placeholder="+7" name="phone" id="form_phone" data-error="">
+		<div class="message">Вы не указали телефон!</div>
+	</div>
+
+	<!--Checkbox input -->
+	<div class="form-box__input-wrapper">
+		<label for="agreed">Я принимаю условия!</label>
+		<input type="checkbox" name="agreed" id="agreed">
+		<div class="message">Вы не приняли соглашение!</div>
+	</div>
+
+	<!--Button-->
+	<div class="form-row form-row-3">
+		<div class="form-col form-col-1">
+			<label class="form-legend"></label>
+			<input type="submit" class="form-button custom-button custom-button-large"
+				   value="Отправить сообщение">
+		</div>
+	</div>
+</form>
+<script src="jquery-3.2.1.min.js"></script>
+<script src="jquery.inputmask.bundle.min.js"></script>
+<script src="script.js"></script>
+</body>
+</html>
---
+++ app/_forms/forms-4 constructor/script.js
@@ -0,0 +1,150 @@
+/*
+* 1. ПРИ НАБОРЕ onchange, проверяем что не постое, и что значение верное
+*
+* 2. ПРИ ОТПРАВКЕ onsubmit, проверяем что не постое, и что значение верное
+*
+* 3. Если верно, отправляем через AJAX на сервер
+*
+*
+* */
+(function () {
+
+	//Конструктор формы
+	function Validator(settings) {
+		this.form = document.getElementById(settings.id);
+		this.elements = this.form.elements;
+		this.submit = this.form.getElementsByClassName(settings.submit)[0];
+		this.mailer = settings.phpScript;
+
+		//Отправляет почту
+		this.mailPost = function(){
+			var recuest = new XMLHttpRequest();
+
+		};
+
+		//Проверяет value текущего элемента на пустую строку
+		function isEmpty(input) {
+
+			if(input.type === "checkbox") {
+				if(!input.checked){
+					input.parentNode.classList.add('error');
+					return false;
+				}else {
+					input.parentNode.classList.remove('error');
+					return true;
+				}
+			}
+
+			if (input.value === "" || input.value === undefined){
+				input.parentNode.classList.add('error');
+				return false;
+			} else {
+				input.parentNode.classList.remove('error');
+				return true;
+			}
+		}
+
+		//Перебор элементов формы
+		for(var i = 0; i < this.elements.length; i++){
+
+			if(this.elements[i].type === "submit") continue;
+			if(this.elements[i].type === "file") {
+				var parent = this.elements[i].parentNode,
+					label = parent.getElementsByTagName('label')[0];
+
+
+				this.elements[i].addEventListener('change', function () {
+
+					var span = this.parentNode.getElementsByTagName('span')[0];
+					span.innerHTML= this.files[0].name;
+
+					console.log(this.files);
+
+				});
+			}
+			if(this.elements[i].type === 'text'){
+				this.elements[i].addEventListener('change', function () {
+
+					isEmpty(this);
+
+				});
+			}
+
+		}
+
+		//Действие при нажатии на кнопку
+		this.onSubmit = function() {
+			var self = this;
+			this.submit.addEventListener('click', function (e) {
+				e.preventDefault();
+				e.stopPropagation();
+				var request = new XMLHttpRequest(),
+					value;
+
+				for(var i = 0; i < self.elements.length; i++){
+
+					if(self.elements[i].type === "submit") continue;
+					if(self.elements[i].type === "checkbox") {
+						if(isEmpty(self.elements[i])){
+							continue;
+						}else {
+							isEmpty(self.elements[i]);
+							return false;
+						}
+					};
+					if(self.elements[i].type === 'text'){
+						if(isEmpty(self.elements[i])){
+							continue;
+						} else {
+							isEmpty(self.elements[i]);
+							return false;
+						}
+					}
+
+
+				}
+
+				//Собирает значения с полей
+				value = new FormData(self.form);
+
+				//Отправляем
+				request.open('POST',self.mailer);
+				request.onreadystatechange = function () {
+					if (request.readyState === 4 && request.status === 200){
+						console.log(request);
+
+					}
+				};
+				// console.log(self.form.elements);
+
+				request.send(value);
+
+			});
+		};
+		this.onSubmit();
+	}
+
+
+	document.addEventListener('DOMContentLoaded', function () {
+
+		//Validation
+		var inputModalPhone = document.getElementById('form_phone');
+		var maskPhone = new Inputmask("+7(999)999-99-99");
+		maskPhone.mask(inputModalPhone);
+
+
+		//
+		var form = new Validator({
+
+			//Id forms
+			id: "bottom_form",
+			//Id button
+			submit: "form-button",
+			phpScript: "mailer.php"
+		});
+
+	});
+
+
+
+})();
